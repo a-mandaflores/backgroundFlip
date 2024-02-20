eae7a477850e61c7ad00952dbf45badb4641ed38
a-mandaflores <nanda.hamandadesouza@gmail.com>
2024-02-20 00:08:08 -0300

    Novos aprendizados, projeto pessoal

---
+++ src/components/BackgroundFlip.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="background">
+    Background-color: <span :style="{color: change}">{{ change }}</span>
+  </div>
+  <button class="button" @click="novaCor">Click</button>
+</template>
+
+<script>
+
+export default {
+  name: 'BackgroundFlip',
+  props: {},
+  emits: ['clicou'],
+  data() {
+    return {
+      change: "rgb(146, 166, 255)"
+    }
+  },
+  methods:{
+    novaCor(){
+     
+      const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+      
+      this.change = randomColor;
+
+      this.$emit('clicou', randomColor)
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.background{
+  color: white;
+  font-weight: 500;
+  border-radius: 5px;
+  padding: 1.5rem;
+  background-color: rgb(0, 0, 0, .5);
+  margin-top: 15%;
+}
+
+.button{
+  border-radius: 5px;
+  color: #333;
+  margin-top: 5%;
+  padding: .8rem;
+  font-weight: 500;
+  border: none;
+  background-color: #fff;
+  transition: .3s ease-in-out;
+}
+
+.button:hover{
+  cursor: pointer;
+  transform: scale(1.05);
+}
+.button:focus{
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.5);
+}
+</style>
---
+++ src/components/MenuSimple.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="back">
+        <div class="container">
+            <h1>Color<span :style="{color:colors}">Flip</span></h1>
+            <ul>
+                <li>Hex</li>
+                <li>Color</li>
+                <li>RGB</li>
+            </ul>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MenuSimple",
+    props: ['colors']
+}
+</script>
+
+<style scoped>
+.back{
+    background-color: rgb(146, 166, 255);
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.container{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 700px;
+    height: 60px;
+}
+.container span{
+    color: black;
+}
+
+.container h1 {
+    padding-left: 1rem ;
+    color: white;
+}
+
+.container ul{
+    display: flex;
+    color: white;
+    list-style: none;
+    
+}
+
+.container ul li {
+    cursor: pointer;
+    margin: 1rem;
+    transition: .3s ease-in-out;
+}
+
+.container ul li:hover{
+    transform: scale(1.1);
+    font-weight: 600;
+    border-bottom: 1px solid white;
+}
+.container ul li:focus{
+    transform: scale(1.1);
+    font-weight: 600;
+}
+</style>
